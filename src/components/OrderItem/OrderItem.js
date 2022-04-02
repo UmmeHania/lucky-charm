04b4b3b62dfb3d7d3340c8a22fe3d2b1de188387
UmmeHania <haniazoly@gmail.com>
2022-04-03 02:56:48 +0600

    cart item added

--- src/components/OrderItem/OrderItem.js
+++ src/components/OrderItem/OrderItem.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BsTrash } from "react-icons/bs";
 
-const OrderItem = () => {
-  return <div></div>;
+const OrderItem = ({ order }) => {
+  const { name, img } = order;
+  const clicked = () => {
+    console.log("Clicked");
+  };
+  return (
+    <div>
+      <div className="d-flex justify-content-start m-3">
+        <img src={img} height="50px" alt="" />
+        <p className="m-3">{name}</p>
+        <button onClick={clicked} className="btn btn-outline-danger border-0">
+          <BsTrash></BsTrash>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default OrderItem;
